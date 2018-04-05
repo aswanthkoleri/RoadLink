79f6049b3eda172805cbd6b073db80e82f02ee5a
aswanthkoleri <iit2016105@iiita.ac.in>
2018-04-05 18:02:02 +0530

    Add login and signup button

--- account/forms.py
+++ account/forms.py
@@ -4,6 +4,7 @@ from django.contrib.auth.forms import UserCreationForm
 
 class RegistrationForm(UserCreationForm):
     email=forms.EmailField(required=True)
+    phone=forms.CharField(required=True)
     class Meta:
         model = User
         fields = [
@@ -11,23 +12,17 @@ class RegistrationForm(UserCreationForm):
             'first_name',
             'last_name',
             'email',
+            'phone',
             'password1',
             'password2'
         ]
-        widgets = {
-            'username' : forms.TextInput(attrs={'class' : 'form-control'}),
-            'first_name' : forms.TextInput(attrs={'class' : 'form-control'}),
-            'last_name' : forms.TextInput(attrs={'class' : 'form-control'}),
-            'email' : forms.EmailInput(attrs={'class' : 'form-control'}),
-            'password1'  : forms.PasswordInput(attrs={'class' : 'form-control'}),
-            'password2' : forms.PasswordInput(attrs={'class' : 'form-control'}),
-        }
 
     def save(self, commit=True):
         user = super(RegistrationForm,self).save(commit=False)
         user.first_name=self.cleaned_data['first_name']
         user.last_name=self.cleaned_data['last_name']
         user.email=self.cleaned_data['email']
+        user.phone=self.cleaned_data['phone']
         if commit:
             user.save()
 
