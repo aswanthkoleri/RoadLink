d011b56601dcbf890a92133ab69687c1211622b5
aswinzz <iit2016106@iiita.ac.in>
2018-04-14 02:54:05 +0530

    add driver feature when booked

--- driver/models.py
+++ driver/models.py
@@ -8,6 +8,14 @@ class Driver(models.Model):
     email=models.CharField(max_length=300)
     phoneNumber=models.CharField(max_length=10)
     licenseCategory=models.CharField(max_length=100)
-
+    status_CHOICES=(
+        ('B','booked'),
+        ('NB','not booked')
+    )
+    status=models.CharField(
+        max_length=2,
+        choices=status_CHOICES,
+        default='NB',
+    )
     def __str__(self):
         return 'Driver name : '+self.firstName
