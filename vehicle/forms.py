01ec4a201367a128dc8ebc62c30ae92b11084d69
aswanthkoleri <iit2016105@iiita.ac.in>
2018-04-17 01:48:21 +0530

    Apply navbar change to Vehicle App

--- vehicle/forms.py
+++ vehicle/forms.py
@@ -4,5 +4,4 @@ from .models import Vehicle
 class VehicleForm(forms.ModelForm):
     class Meta:
         model=Vehicle
-        fields = ('cost_per_km','price','registration_plate','insurance_status',
-        'no_of_km_travelled','fuel_type','mileage','vehicle_type','image')
+        fields = ('cost_per_km','price','registration_plate','no_of_km_travelled','mileage','vehicle_type','fuel_type','insurance_status','image')
