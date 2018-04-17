2f7dc44b4375a552fa8f4ee4e42a613dc5163f3a
aswinzz <iit2016106@iiita.ac.in>
2018-04-17 16:00:19 +0530

    add payment gateway

--- booking/urls.py
+++ booking/urls.py
@@ -5,6 +5,7 @@ urlpatterns = [
     path('', views.index,name='index'),
     path('book',views.book,name='book'),
     path('bookings',views.booking,name='bookings'),
+    path('pay',views.pay,name='pay'),
     path('delete/<int:id>',views.delete,name='delete'),
     path('changestatus/<int:id>',views.change,name='delete'),
 ]
